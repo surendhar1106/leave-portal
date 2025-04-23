1b3dbfb82f2ee65987104b191d7ba9ebedda3d24
NithiswerS <nithiswerr@gmail.com>
2025-04-23 11:20:24 +0530

    added test login

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 
 app = Flask(__name__)
+
+app.secret_key = 'y'
+
 
 @app.route('/')
 def index():
@@ -16,6 +19,7 @@ def login():
     stored_password = '123789'
 
     if username == stored_username and password == stored_password:
+        session['logged_in'] = True
         return redirect(url_for('dashboard'))
     
     else:
@@ -24,7 +28,20 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    if 'logged_in' in session and session['logged_in']:
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('index'))
+    
+
+@app.route('/submitrequest', methods=['POST'])
+def submitrequest():
+    reason = request.form['reason']
+    from_date = request.form['from_date']
+    to_date = request.form['to_date']
+
+    print(f"reason:{reason},date:{from_date}:{to_date}")
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
